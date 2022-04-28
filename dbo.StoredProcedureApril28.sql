ba332f4e62d16c9d22cf300a8c1b7de63c85d45d
karankumar02057 <90321456+karankumar02057@users.noreply.github.com>
2022-04-28 15:19:04 +0530

    Create dbo.StoredProcedureApril28.sql

---
+++ dbo.StoredProcedureApril28.sql
@@ -0,0 +1,24 @@
+-- =======================================================
+-- Create Stored Procedure Template for Azure SQL Database
+-- =======================================================
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
+-- Author:      <Author, , Name>
+-- Create Date: <Create Date, , >
+-- Description: <Description, , >
+-- =============================================
+CREATE PROCEDURE StoredProcedureApril28
+
+AS
+BEGIN
+    -- SET NOCOUNT ON added to prevent extra result sets from
+    -- interfering with SELECT statements.
+    SET NOCOUNT ON
+
+    -- Insert statements for procedure here
+    SELECT * From dbo.Students
+END
+GO
